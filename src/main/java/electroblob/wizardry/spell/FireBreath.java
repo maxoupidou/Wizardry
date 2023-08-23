aa818c30d6ed66ea99e1e88a6973ff92a2cf2040
Max <76659964+maxoupidou@users.noreply.github.com>
2023-08-24 00:12:28 +0200

    Update FireBreath.java
    Add if(Wizardry.settings.allowSpellsToPlaceFireBlock) condition to place flames

--- src/main/java/electroblob/wizardry/spell/FireBreath.java
+++ src/main/java/electroblob/wizardry/spell/FireBreath.java
@@ -1,5 +1,6 @@
 package electroblob.wizardry.spell;
 
+import electroblob.wizardry.Wizardry;
 import electroblob.wizardry.item.SpellActions;
 import electroblob.wizardry.registry.WizardryItems;
 import electroblob.wizardry.util.*;
@@ -72,7 +73,9 @@ public class FireBreath extends SpellRay {
 		pos = pos.offset(side);
 
 		if(world.isAirBlock(pos)){
-			if(!world.isRemote && BlockUtils.canPlaceBlock(caster, world, pos)) world.setBlockState(pos, Blocks.FIRE.getDefaultState());
+			if(!world.isRemote && BlockUtils.canPlaceBlock(caster, world, pos)){
+				if(Wizardry.settings.allowSpellsToPlaceFireBlock){world.setBlockState(pos, Blocks.FIRE.getDefaultState());}
+			}
 			return true;
 		}
 		
